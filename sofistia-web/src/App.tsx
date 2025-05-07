fa453fb839b93ab87090fd0f2423c747e0c90e20
J-medeiros <joao.v.m.s.jacinto@academico.unirv.edu.br>
2025-05-07 17:46:16 -0300

    Implementação do sistema funcional não conectado ao back end

--- sofistia-web/src/App.tsx
+++ sofistia-web/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Orders from "./page/Orders";
+import Kitchen from "./page/Kitchen";
+import Home from "./page/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kitchen" element={<Kitchen />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
